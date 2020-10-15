3dd31eadbcdefa4de8f5d3df694b398a762c1ff9
俊华 蔡 <1032031912@qq.com>
2020-10-15 15:33:00 +0800

    添加项目文件。

---
+++ 01_TCP客户端/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace _01_TCP客户端
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(506, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "连接服务器";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(367, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 33);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(287, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "端口号:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(101, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 33);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "服务器IP:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(8, 80);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(625, 183);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(8, 298);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(625, 92);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(3, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 26);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "消息列表:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(3, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "消息编辑:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(428, 396);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 33);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "发 送";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(536, 396);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 33);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "清 空";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 441);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 10_15_TCP服务器/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_15_TCP服务器
+{
+    public partial class Form1 : Form
+    {
+        //创建全局变量
+        Socket _ServerSocket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //启动服务
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //拿到IP地址还有端口号
+            string _serverIP = this.textBox1.Text;
+            string _serverPort = this.textBox2.Text;
+
+            //初始化Socket(全局性)对象
+            //AddressFamily.InterNetwork   使用IP4协议       
+            //SocketType.Stream                  支持双向字节流
+            //ProtocolType.Tcp                     使用TCP协议
+            _ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //将拿到的字符串转换成IP地址与端口号
+            IPAddress IP = IPAddress.Parse(_serverIP);
+            int Port = Convert.ToInt32(_serverPort);
+            //初始化形成IP地址
+            IPEndPoint iPEnd = new IPEndPoint(IP, Port);
+
+            //设置Socket属性
+            _ServerSocket.Bind(iPEnd); //绑定IP地址与端口号
+            _ServerSocket.Listen(10);   //设定监听最多10个客户端
+
+            //提示启动服务器成功
+            this.listBox1.Items.Add("启动服务器成功!");
+
+            //启动监控线程,用以监听是否有客户端连接过来（监狱接待大厅的作用）
+            Thread listenThread = new Thread(ListenClientConnect);
+            listenThread.IsBackground = true;//后台线程
+            listenThread.Start();                       //线程启动
+        }
+        //定义一个用于实现监听的全局变量
+        Socket clientSocket;
+        //创建一个字典用于接收连接过来的客户端IP与对象
+        Dictionary<string, Socket> ascok = new Dictionary<string, Socket>();
+        public void ListenClientConnect()
+        {
+            while (true)
+            {
+                //会对当前线程一直阻塞直到客户端连接过来 [客户端连接过来时解除线程阻塞]
+                clientSocket = _ServerSocket.Accept();//引发线程柱塞
+
+                //如果能到这一步，说明有客户端连接过来，此时利用字典保存对象与名称
+                ascok.Add(clientSocket.RemoteEndPoint.ToString(), clientSocket);
+
+                //往下拉列表中添加 IP：端口号  => 用以后面的选择客户端发送消息
+                this.comboBox1.Items.Add(clientSocket.RemoteEndPoint.ToString());
+
+                //往消息列表中添加一行内容
+                this.listBox1.Items.Add(clientSocket.RemoteEndPoint.ToString()+"链接成功");
+
+                //发送消息给连接上的客户端,提示已经连接上服务器
+                clientSocket.Send(Encoding.UTF8.GetBytes("成功连接上服务器！"));
+
+                //启动会话线程用户接受发送消息
+                Thread ReceiveThread = new Thread(()=>ReceiveMessage(clientSocket));
+                ReceiveThread.IsBackground = true;
+                ReceiveThread.Start();
+            }   
+        }
+
+
+        //创建全局缓存变量
+        private static byte[] _result = new byte[1024];
+        //线程的功能函数=> 回复与发送
+        public void ReceiveMessage(Socket myClientSocket)
+        {
+            //转换Socket  里氏转换
+            while (true)
+            {
+                //通过 myClientSocket 接收数据
+                int resultNumber = myClientSocket.Receive(_result);
+                //判断接收的内容是否大于0，如果小于0说明没有内容
+                if (resultNumber>0)
+                {
+                    //拼接字符串，形成消息
+                    string str = string.Format("接收客户端{0} 消息{1} \r\n", myClientSocket.RemoteEndPoint.ToString(), Encoding.UTF8.GetString(_result, 0, resultNumber));
+                    //添加到消息列表当中
+                    this.listBox1.Items.Add(str);
+                }
+            }
+
+        }
+
+
+    }
+}
